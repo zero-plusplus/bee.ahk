b9ad37a260cb951de7fe9e364a2441388d289250
zero-plusplus <git@zero-plusplus.com>
2021-10-30 18:41:16 +0900

    Delegate inliner to external

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,19 +1,20 @@
+import { promises as fs } from 'fs';
 import * as gulp from 'gulp';
 import * as del from 'del';
 import * as mkdirp from 'mkdirp';
-import { AhkInliner } from './external/AhkInliner';
+import { AhkVersion, IncludeInliner } from '@zero-plusplus/autohotkey-utilities';
+// import { AhkInliner } from './external/AhkInliner';
 
 const buildDir = './build';
 export const clean = async(): Promise<void> => {
   await del(buildDir);
 };
 export const build_v2 = async(): Promise<void> => {
-  const ahkVersion_2 = 2;
-  const inliner_v2 = new AhkInliner(ahkVersion_2);
+  const inliner_v2 = new IncludeInliner(new AhkVersion('2.0'));
   mkdirp.sync(buildDir);
-  inliner_v2.write('./src/bee.ahk2', './build/bee.ahk2');
 
-  return Promise.resolve();
+  const source = inliner_v2.exec('./src/bee.ahk2');
+  return fs.writeFile('./build/bee.ahk2', source, 'utf-8');
 };
 export const build = gulp.series(clean, build_v2);
 
